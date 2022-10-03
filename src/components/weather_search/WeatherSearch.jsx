1834ee89442e616bbabcd032d2a70fe25c1f76e3
Waris <warisaremou2003@gmail.com>
2022-10-03 23:11:36 +0200

    Création de la fonction weatherDetails

--- src/components/weather_search/WeatherSearch.jsx
+++ src/components/weather_search/WeatherSearch.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import { useState } from 'react'
 import './weatherSearch.css'
 import searchIcon from '../../assets/search.svg'
 
 function WeatherSearch() {
+
+    const apiKey = '32ebb4b35f6eb0f1f526e2e9c5010e51'
+    // const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+    const [inputValue, setInputValue] = useState('')
+    const [country, setCountry] = useState([])
+
+    function keyPressed(e) {
+        if (e.keyCode === 13) {
+            // console.log(inputValue)
+            setCountry(currentValue => [...currentValue, inputValue])
+            searchWeather(inputValue)
+            setInputValue('')
+        }
+    }
+
+    function searchWeather(city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+            .then(
+                response => response.json()
+            ).then(
+                result => weatherDetails(result)
+            )
+    }
+
+    function weatherDetails(cityName) {
+        console.log(cityName)
+
+        cityName.cod === '404' ? console.log("Erreur !")
+        :(
+            console.log(cityName.cod)
+        )
+    }
+
     return (
         <div id="weather-search">
-            <input type="text" name="country" id="country" placeholder='Enter country name' />
+            <input type="text" value={inputValue} id="country"
+                placeholder='Enter country name' onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={keyPressed}
+            />
             <div className="search-icon">
                 <img src={searchIcon} alt="search-icon" />
             </div>
@@ -13,16 +51,11 @@ function WeatherSearch() {
             <div className="recent-searches">
                 <h5>Recent searches</h5>
                 <div className="searches-list">
-                    <p>London</p>
-                    <p>London</p>
-                    <p>London</p>
-                    <p>London</p>
-                    <p>London</p>
-                    <p>London</p>
-                    <p>London</p>
-                    <p>London</p>
-                    <p>London</p>
-                    <p>London</p>
+                    {country.map((countryName, index) => (
+                        <p key={`${countryName}-${index}`} >
+                            {countryName.charAt(0).toUpperCase() + countryName.slice(1)}
+                        </p>
+                    ))}
                 </div>
             </div>
 
