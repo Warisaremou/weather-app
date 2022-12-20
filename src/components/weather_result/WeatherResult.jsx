b1205a22e141e1c1134334d1df32cf6f68c841f1
waris <warisaremou2003@gmail.com>
2022-12-20 10:19:48 +0100

    updating date and time

--- src/components/weather_result/WeatherResult.jsx
+++ src/components/weather_result/WeatherResult.jsx
@@ -6,6 +6,11 @@ function WeatherResult({ countryDetails, setCountryDetails }) {
 
     const imgUrl = `http://openweathermap.org/img/w/${countryDetails.weather ? countryDetails.weather[0].icon : null}.png`
 
+    function setDate(dt) {
+        var day = new Date(dt * 1000)
+        return day.toUTCString()
+    }
+
     return (
         <div id="weather-result">
             <div className="weather-logo">
@@ -16,7 +21,7 @@ function WeatherResult({ countryDetails, setCountryDetails }) {
                 <p>{`${countryDetails.wind ? `${countryDetails.wind.deg}` : 0}%`}</p>
                 <div className="other-details">
                     <h3>{countryDetails.name ? countryDetails.name : "-"}</h3>
-                    <p>10:36 - Tuesday,Oct'19</p>
+                    <p>{countryDetails.dt ? setDate(countryDetails.dt) : "Tue,19 Dec 2022 05:06:52 GMT" }</p>
                 </div>
                 <div className="weather-icon">
                     <img src={imgUrl} alt="icon" width="40px" height="40px" />
